343beb2c00685691e226b40c2afef09e2a2067a0
faraz <faraz.fatahnaie@gmail.com>
2023-07-23 15:27:42 +0330

    delete older settings.py and add new setting.py to ./config/

---
+++ configs/setting.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import json
+
+
+def setting(config_file=None):
+    config = dict()
+    BASE_DIR = Path(__file__).resolve().parent.parent
+
+    if config_file is None:
+        config_name = 'CONFIG1'  # config file name in config dir
+        config_dir = BASE_DIR.joinpath('configs')
+        config_file = open(f'{config_dir}/{config_name}.json')
+        config_file = json.load(config_file)
+
+    # DATASET_DIR = BASE_DIR.joinpath('dataset')
+    # DATASET_NAME = config_file['dataset']['name']
+    # DATASET_PATH = DATASET_DIR.joinpath(DATASET_NAME)
+    config['DATASET_PATH'] = '/home/faraz/PycharmProjects/IDS/dataset/NSL_KDD/file/preprocessed'
+    config['DATASET_TYPE'] = config_file['dataset']['type']
+
+    config['NUM_WORKER'] = config_file['dataset']['n_worker']
+    config['MODEL_NAME'] = config_file['model']['name']
+
+    config['LOSS_FUNCTION'] = config_file['model']['loss']
+    config['EPOCHS'] = config_file['model']['epoch']
+    config['BATCH_SIZE'] = config_file['model']['batch_size']
+    config['OPTIMIZER'] = config_file['model']['optim']
+    config['LR'] = config_file['model']['lr']
+    config['WEIGHT_DECAY'] = config_file['model']['weight_decay']
+    config['SCHEDULER'] = config_file['model']['scheduler']
+    config['MIN_LR'] = config_file['model']['min_lr']
+    config['PATIENCE'] = config_file['model']['patience']
+    config['EARLY_STOP'] = config_file['model']['early_stop']
+    config['FACTOR'] = config_file['model']['factor']
+    config['GAMMA'] = config_file['model']['gamma']
+    config['REGULARIZATION'] = config_file['model']['regularization']
+
+    return config, config_file
