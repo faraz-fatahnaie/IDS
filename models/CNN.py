72e900efde8e800ff9166b154b0fe36e77b34f89
faraz <faraz.fatahnaie@gmail.com>
2023-08-02 16:54:04 +0330

    adding classification mode for building models

--- models/CNN.py
+++ models/CNN.py
@@ -5,9 +5,14 @@ from torchsummary import summary
 
 
 class CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, classification_mode: str = 'multi'):
         super(CNN, self).__init__()
-        self.n_classes = 5
+        if classification_mode == 'multi':
+            self.n_classes = 5
+            self.activation = nn.Softmax(dim=1)
+        elif classification_mode == 'binary':
+            self.n_classes = 1
+            self.activation = nn.Sigmoid()
         self.conv2d_1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=2, stride=1)
         self.conv2d_2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=2, stride=1)
         self.conv2d_3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, stride=1)
@@ -20,8 +25,7 @@ class CNN(nn.Module):
         self.gap = nn.AdaptiveAvgPool2d(1)
 
     def forward(self, x):
-        y = x
-        y = self.conv2d_1(y)
+        y = self.conv2d_1(x)
         y = self.bn_1(y)
         y = F.relu(y)
         y = self.maxpool2d_1(y)
@@ -36,17 +40,15 @@ class CNN(nn.Module):
         y = F.relu(y)
 
         y = self.gap(y)
-        #print(y.size()[1])
         y = nn.Flatten()(y)
-        #print(y.size()[1])
         y = self.linear(y)
-        y = F.softmax(y, dim=1)
+        y = self.activation(y)
 
         return y
 
 
 if __name__ == '__main__':
-    #summary(model=SEForward(), input_size=(1, 11, 11))
+    # summary(model=SEForward(), input_size=(1, 11, 11))
     input = torch.randn(1, 1, 11, 11)
     se = CNN()
     output = se(input)
--- models/SE.py
+++ models/SE.py
@@ -29,9 +29,15 @@ class SEBlock2D(nn.Module):
 
 
 class SE(nn.Module):
-    def __init__(self):
+    def __init__(self, classification_mode):
         super(SE, self).__init__()
-        self.n_classes = 5
+        if classification_mode == 'multi':
+            self.n_classes = 5
+            self.activation = nn.Softmax(dim=1)
+        elif classification_mode == 'binary':
+            self.n_classes = 1
+            self.activation = nn.Sigmoid()
+
         self.SE = nn.Sequential(
 
             nn.Conv2d(in_channels=1, out_channels=8, kernel_size=2, stride=1),
@@ -42,59 +48,60 @@ class SE(nn.Module):
             nn.BatchNorm2d(16),
             nn.ReLU(True),
             nn.Flatten(),
-            nn.Dropout(0.1),
-            nn.Linear(in_features=1024, out_features=self.n_classes),
-            nn.Softmax(dim=1)
+            nn.Dropout(0.1)
+
         )
+        self.fc = nn.Linear(in_features=1936, out_features=self.n_classes)
 
     def forward(self, x):
-        return self.SE(x)
+        out = self.SE(x)
+        out = self.fc(out)
+
+        return self.activation(out)
+
+# class SE(nn.Module):
+#     def __init__(self):
+#         super(SE, self).__init__()
+#         self.n_classes = 5
+#         self.Conv2d_1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=2, stride=1)
+#         self.Conv2d_2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1)
+#         self.Conv2d_3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1, stride=1)
+#         self.SEBlock2D_1 = SEBlock2D(in_channels=8, ratio=2)
+#         self.SEBlock2D_2 = SEBlock2D(in_channels=16, ratio=2)
+#         self.SEBlock2D_3 = SEBlock2D(in_channels=64, ratio=2)
+#         self.linear1 = nn.Linear(in_features=1024, out_features=self.n_classes)
+#         #self.linear2 = nn.Linear(in_features=128, out_features=self.n_classes)
+#         # self.relu = nn.ReLU(True)
+#         # self.softmax = nn.Softmax(dim=1)
+#
+#     def forward(self, x):
+#
+#         y = self.Conv2d_1(x)
+#         y = F.relu(y)
+#         y = self.SEBlock2D_1(y)
+#         y = self.Conv2d_2(y)
+#         y = self.SEBlock2D_2(y)
+#         y = F.relu(y)
+#         #y = self.SEBlock2D_3(y)
+#         #print(y.size())
+#         #y = y.mean(dim=(-2, -1))
+#         y = nn.BatchNorm2d(16)(y)
+#         y = F.relu(y)
+#
+#         y = nn.Flatten()(y)
+#         #print(y.size())
+#         y = self.linear1(y)
+#         #y = self.linear2(y)
+#         #print(y.size())
+#         y = F.softmax(y, dim=1)
+#         #torch.softmax(y, dim=1)
+#
+#         return y
 
 
-class SEForward(nn.Module):
-    def __init__(self):
-        super(SEForward, self).__init__()
-        self.n_classes = 5
-        self.Conv2d_1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=2, stride=1)
-        self.Conv2d_2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1)
-        self.Conv2d_3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1, stride=1)
-        self.SEBlock2D_1 = SEBlock2D(in_channels=8, ratio=2)
-        self.SEBlock2D_2 = SEBlock2D(in_channels=16, ratio=2)
-        self.SEBlock2D_3 = SEBlock2D(in_channels=64, ratio=2)
-        self.linear1 = nn.Linear(in_features=1024, out_features=self.n_classes)
-        #self.linear2 = nn.Linear(in_features=128, out_features=self.n_classes)
-        # self.relu = nn.ReLU(True)
-        # self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, x):
-
-        y = self.Conv2d_1(x)
-        y = F.relu(y)
-        y = self.SEBlock2D_1(y)
-        y = self.Conv2d_2(y)
-        y = self.SEBlock2D_2(y)
-        y = F.relu(y)
-        #y = self.SEBlock2D_3(y)
-        #print(y.size())
-        #y = y.mean(dim=(-2, -1))
-        y = nn.BatchNorm2d(16)(y)
-        y = F.relu(y)
-
-        y = nn.Flatten()(y)
-        #print(y.size())
-        y = self.linear1(y)
-        #y = self.linear2(y)
-        #print(y.size())
-        y = F.softmax(y, dim=1)
-        #torch.softmax(y, dim=1)
-
-        return y
-
-
-
-#if __name__ == '__main__':
-#    #summary(model=SEForward(), input_size=(1, 11, 11))
+# if __name__ == '__main__':
+#    summary(model=SE(), input_size=(1, 11, 11))
 #    input = torch.randn(1, 1, 11, 11)
-#    se = SEForward()
+#    se = SE()
 #    output = se(input)
 #    #print(output.shape)
